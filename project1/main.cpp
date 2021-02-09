502c5a484e9c9432cc8b42dbd269c562e340fd68
Jackie <chowdhury1478@gmail.com>
2021-02-08 19:58:27 -0500

    queue insert and delete functioning

--- project1/main.cpp
+++ project1/main.cpp
@@ -30,7 +30,7 @@ class listNode{
         
     }
 };
-
+//--------------------- LLStack -------------------------//
 class LLstack{
     private:
     
@@ -112,6 +112,71 @@ class LLstack{
        
     }
 };
+//-------------------------- LLQueue ---------------------------//
+
+class LLqueue{
+    private:
+    int size = 0;
+    public:
+    listNode* head;// head always points to the dummy node!
+    listNode* tail;// tail always points to the last node of the queue; tail points to dummy initially.
+
+    // creates a new Queue with a dummy node, set the data in the dummy node to -9999 and sets next to null;
+    //let both head and tail point to the dummy node.
+    LLqueue(){
+        listNode* dummy = new listNode(-9999);
+        dummy->next = NULL;
+        head = dummy;
+        tail= dummy;
+    }
+
+    void buildQueue(ifstream &inFile, ofstream &outFile){
+
+    }
+
+    //head is always dummy. head's next is 1st one out. tail is last one out.
+    // insert the newNode after the tail of Q.
+    void insertQ(listNode* node){
+        listNode* temp = head;
+        while(head->next != NULL){
+            head = head->next;
+        }
+        head->next = node;
+        tail = node;
+        head = temp;
+        size++;
+    }
+
+    // if Q is not empty, deletes the first node, after the dummy from Q and returns the
+    // deleted node, otherwise, returns null.
+    listNode* deleteQ(){
+        listNode* temp = new listNode(-9999);
+        temp = head->next;
+        if(isEmpty()){
+            return NULL;
+        }else{
+            head->next = head->next->next;
+            size--;
+        }
+
+        return temp;
+    }
+    // checks to see if Q is empty, i.e., tail points to the dummy node. It returns true is Q is
+    //empty and false otherwise.
+    bool isEmpty(){
+        if(size==0){
+            return true;
+        }
+        return false;
+    }
+
+    // DOES NOT delete nodes in Queue! Outputs to outFile2, the entire Q, including the dummy
+    //node, you may call printNode (...) using the following format:
+    void printQ(ofstream &outFile){
+
+    }
+
+};
 
 int main(int argc, char* argv[]){
 
@@ -133,10 +198,22 @@ int main(int argc, char* argv[]){
 
    LLstack* stk = new LLstack();
    stk->buildStack(input,output);
+   input.close();
+   output.close();
+
+   LLqueue* que = new LLqueue();
+   que->insertQ(new listNode(1));
+   que->insertQ(new listNode(2));
+   que->insertQ(new listNode(3));
+   que->insertQ(new listNode(4));
+   que->insertQ(new listNode(5));
+
+   while(!que->isEmpty()){
+       cout<< que->deleteQ()->val <<endl;
+   }
 
 
-    input.close();
-    output.close();
+
     
 
 }
