eec4cd1fd2d3bc9502f02fa96587143fff3f1a95
Jackie <chowdhury1478@gmail.com>
2021-02-20 01:52:59 -0500

    project2 done

--- project2/main.cpp
+++ project2/main.cpp
@@ -22,9 +22,9 @@ class listNode{
     void printNode(ofstream &outFile){
         //(this node's firstName, this node’s lastName, next node’s firstName ) 
         if(next != NULL){
-            outFile << "(" <<firstName << " " << lastName << " " << next->firstName << ")-->"<< endl;
+            outFile << "(" <<firstName << " " << lastName << " " << next->firstName << ")-->";
         }else{
-            outFile << "(" <<firstName << " " << lastName << " " << "NULL" << ")-->"<< endl;
+            outFile << "(" <<firstName << " " << lastName << " " << "NULL" << ")-->NULL";
         }
         
     }
@@ -69,12 +69,34 @@ class hashTable{
     }
 
     // see algorithm below. FINISH LAST
-    void informationProcessing(ifstream &inFile,ofstream &outFile){
+    void informationProcessing(ifstream &inFile,ofstream &outFile2){
+        char openingBlankSpot;
+        char op;
+        string firstName,lastName;
+        int index;
+        //ifream add three black characters at the start of the document, this helps fix it. 
+        inFile>>openingBlankSpot >> openingBlankSpot >>openingBlankSpot;
+        while(!inFile.eof()){
+            inFile >> op >> firstName >> lastName;
+            //cout << op << " "<< firstName << " " <<lastName <<endl;
+            outFile2 << op << " " << firstName << " " <<lastName <<" is being processed" << endl;
+            index = doIt(lastName);
+            outFile2 << firstName << " " <<lastName <<" is processing in Hash Table " << index << endl;
+            printList(index,outFile2);
 
+            if(op == '+'){
+                hashInsert(index,firstName,lastName,outFile2);
+            }else if(op == '-'){
+                hashDelete(index,firstName,lastName,outFile2);
+            }else if(op == '?'){
+                hashRetrieval(index,firstName,lastName,outFile2);
+            }
+        }
     } 
 
     // search thru hashTable[index] linked list to locate the
     //record with firstName and lastName. SEE EMAIL FOR ALGORITHM.
+    //returns the previous node of the matching node.
     listNode* findSpot(int index, string firstName, string lastName){
         listNode* spot = &hashTbl[index];
         //if Spot's next != null *and*  Spot's next's lastName < lastName
@@ -93,7 +115,7 @@ class hashTable{
 
     //see algorithm
     void hashInsert(int index, string firstName,string lastName,ofstream &outFile2){
-        outFile2 << "performing hash insert on " << firstName << " " << lastName << endl;
+        outFile2 << "***Performing hash insert on " << firstName << " " << lastName << endl;
         listNode* spot = findSpot(index,firstName,lastName);
 
         if( spot->next != NULL && spot->next->lastName == lastName && spot->next->firstName == firstName){
@@ -106,27 +128,56 @@ class hashTable{
         }
     }
     // see algorithm 
-    void hashDelete(){
-        
+    void hashDelete(int index, string firstName, string lastName,ofstream &outFile2){
+        outFile2<<"*** Performing delete on " << firstName << " " << lastName << endl;
+        listNode* spot = findSpot(index,firstName,lastName);
+        //if (Spot's next != null *and* Spot’s next’s lastName == lastName *and* Spot’s next’s firstName == firstName)
+        if(spot->next != NULL && spot->next->lastName == lastName && spot->next->firstName == firstName){
+            listNode* junk = spot->next;
+            spot->next = spot->next->next;
+            junk->next = NULL;
+            free(junk);
+            printList(index,outFile2);
+        }else{
+            outFile2<< "***Warning ,the record is *not* in the database!";
+        }
     }
 
     //see alogrithm
-    void hashRetrieval(){
-
+    void hashRetrieval(int index,string firstName,string lastName,ofstream &outFile2){
+        outFile2<<"*** Performing hashRetrieval on "<<firstName << " "<<lastName << endl;
+        listNode* spot = findSpot(index,firstName,lastName);
+        //if Spot's next != null *and* Spot’s next’s lastName == lastName *and* Spot’s next’s firstName == firstName
+        if(spot->next != NULL && spot->next->lastName == lastName && spot->next->firstName==firstName){
+            outFile2<<"Yes, the record is in the database!" <<endl;
+        }else{
+            outFile2<<"No, the record is not in the database!" <<endl;
+        }
     }
     // print the linked list of hashTable [index], use the format given in the above.
     void printList (int index, ofstream &outFile){
-
+        listNode* head = &hashTbl[index];
+        listNode* temp = head;
+        outFile<<"HashTable["<<index<<"]: ";
+        while(head != NULL){
+            head->printNode(outFile);
+            head = head->next;
+        }
+        outFile<<endl;
+        head = temp;
     }
     // output the entire hashTable, call printList (...), index from 0 to bucketSize -1.
-    void printHashTable (ofstream outFile){
+    void printHashTable (ofstream &outFile){
+
+        for(int i = 0; i < bucketSize ;i++){
+            printList(i,outFile);
+        }
     }
 };
 
 int main(int argc, char* argv[]){
-    cout<<"hello" << endl;
     ifstream input;
-    ofstream output1;
+    ofstream output1,output2;
     int size = 0;
 
     input.open(argv[1]);
@@ -139,21 +190,19 @@ int main(int argc, char* argv[]){
     string temp = argv[2];
     size = stoi(temp);
     output1.open(argv[3]);
+    output2.open(argv[4]);
 
 
    hashTable* table = new hashTable(size);
    listNode node;
    table->createHashTable();
-   
-   
-   for(int i = 0; i < size; i++){
-     node  =  table->hashTbl[i];
-     node.printNode(output1);
-     
-   }
+   table->informationProcessing(input,output2);
+   table->printHashTable(output1);
    
    
 
 
     input.close();
+    output1.close();
+    output2.close();
 }
